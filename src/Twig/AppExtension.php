6326132aca547b80b91be6536a5cf838a3e0c3ea
Ra <mr.mallaev@gmail.com>
2019-05-16 16:21:15 +0200

    chore: Install Bootstrap 4 and compile assets using Encore
    Encore not compiling css and js files from node_modules

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,10 +3,21 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+    /**
+     * @var string
+     */
+    private $locale;
+
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
     public function getFilters()
     {
         return [
@@ -17,5 +28,17 @@ class AppExtension extends AbstractExtension
     public function priceFilter($number)
     {
         return '$'.number_format($number, 2, '.', ',');
+    }
+
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return array An array of global variables
+     */
+    public function getGlobals()
+    {
+        return [
+            'locale' => $this->locale
+        ];
     }
 }
