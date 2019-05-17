b0f6c8476a6cca9bf1655667fa8ed170f304dd4b
Ra <mr.mallaev@gmail.com>
2019-05-17 14:33:12 +0200

    feat: Add flashbags when micro-post is deleted, created or modified

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -45,12 +46,18 @@ class MicroPostController
      */
     private $router;
 
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
+
     public function __construct(
         \Twig_Environment $twig,
         MicroPostRepository $microPostRepository,
         FormFactoryInterface $formFactory,
         EntityManagerInterface $entityManager,
-        RouterInterface $router
+        RouterInterface $router,
+        FlashBagInterface $flashBag
     )
     {
         $this->twig = $twig;
@@ -58,6 +65,7 @@ class MicroPostController
         $this->formFactory = $formFactory;
         $this->entityManager = $entityManager;
         $this->router = $router;
+        $this->flashBag = $flashBag;
     }
 
     /**
@@ -83,6 +91,8 @@ class MicroPostController
 
         if($form->isSubmitted() && $form->isValid()){
             $this->entityManager->flush();
+
+            $this->flashBag->add('notice', 'The micro-post was modified.');
 
             return new RedirectResponse(
                 $this->router->generate('micro_post_index')
@@ -114,6 +124,8 @@ class MicroPostController
             $this->entityManager->persist($microPost);
             $this->entityManager->flush();
 
+            $this->flashBag->add('notice', 'Your micro-post was created.');
+
             return new RedirectResponse($this->router->generate('micro_post_index'));
         }
 
@@ -143,6 +155,8 @@ class MicroPostController
     public function delete(MicroPost $microPost){
         $this->entityManager->remove($microPost);
         $this->entityManager->flush();
+
+        $this->flashBag->add('notice', 'The micro-post was successfully deleted');
 
         return new RedirectResponse($this->router->generate(
             'micro_post_index'
