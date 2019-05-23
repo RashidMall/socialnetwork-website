e61f5f64f42629672f9aca2a3405ee992b712b38
Ra <mr.mallaev@gmail.com>
2019-05-23 16:41:20 +0200

    feat: Add method to fetch unseen notifications

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -4,8 +4,10 @@
 namespace App\Controller;
 
 
+use App\Repository\NotificationRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,5 +17,23 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class NotificationController extends AbstractController
 {
+    /**
+     * @var NotificationRepository
+     */
+    private $notificationRepository;
 
+    public function __construct(NotificationRepository $notificationRepository)
+    {
+        $this->notificationRepository = $notificationRepository;
+    }
+
+    /**
+     * @Route("/unseen", name="notification_unseen")
+     */
+    public function unseen(){
+        new JsonResponse([
+            'count_notifications'
+            => $this->notificationRepository->findUnseenByUser($this->getUser())
+        ]);
+    }
 }
